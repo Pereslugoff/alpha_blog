4a0b139ca5b6c35715c9f9617d397357392f98ff
Nick <nickpereslugoff@gmail.com>
2017-12-11 18:25:35 +0000

    Create articles table, model and add validations

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+  validates :title, presence: true, length: { miniumum: 3, maximum: 50 }
+  validates :description, presence: true, length: { miniumum: 10, maximum: 300 }
+end
